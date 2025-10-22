5c9dde4d3c29dcc895ce5dd1273f816c02ab6eda
Viviana Belloso <vivibelloso@hotmail.com>
2025-10-22 12:47:06 -0300

    Corregir uso de await en las funciones de navegación en las pruebas

--- FreeRangePW/tests/test-1.spec.ts
+++ FreeRangePW/tests/test-1.spec.ts
@@ -16,12 +16,12 @@ import { test, Browser, Page, expect } from '@playwright/test';
     for (const seccion of secciones) {
       test(`Validar redirección a la sección "${seccion.nombre}"`, async ({ page }) => {
         await test.step(`Estando yo en la web principal www.freerangetesters.com`, async () => {
-          page.goto('https://www.freerangetesters.com');
+          await page.goto('https://www.freerangetesters.com');
           await expect(page).toHaveTitle('Free Range Testers');
         });
 
         await test.step(`Cuando hago click en "${seccion.nombre}"`, async () => {
-          page.locator('#page_header').getByRole('link', { name: seccion.nombre, exact: true }).click();
+          await page.locator('#page_header').getByRole('link', { name: seccion.nombre, exact: true }).click();
           await page.waitForURL(`**${seccion.url}`);
         });
 
